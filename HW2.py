9b16ec6a0afed04633e1bdc633eac272756b1517
Ashton Gracey <ag127064@student.wpsok.org>
2025-08-20 12:56:50 -0500

    Added HW2

---
+++ HW2.py
@@ -0,0 +1,29 @@
+#Name: Ashton Gracey
+#Class: 5th Hour
+#Assignment: HW2
+
+
+#1. Print "Hello World!"
+print("Helllo World")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+Mardi = 2
+Wall = True
+Waylon = "Jones"
+
+#3. Print all three variables on the same print function (at the same time).
+print("Mardi, Wall, Waylon")
+#4. Create a variable that asks the user to input an integer.
+ashton = (int(input("Insert Number Here")))
+print(ashton)
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+table = (ashton + Mardi)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(table / 2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+Wall = False
+#8. Print the value of the boolean variable.
+print(Wall)
+#9. Create a variable with a number that contains decimals.
+Mouse = 8.9
+#10. Round the number from #9 up or down using the round function.
+print(round(Mouse))
