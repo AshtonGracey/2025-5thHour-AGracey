7e4a8a2da5aa71e87941c70dc5c2e630e85573f7
Ashton Gracey <ag127064@student.wpsok.org>
2025-09-17 12:25:28 -0500

    Added Playground

---
+++ Playground.py
@@ -0,0 +1,19 @@
+#Name: Ashton Gracey
+#Class: 5th Hour
+#Assignment: Playground
+
+import random
+currency = "$500"
+print(currency)
+
+
+slotmachine = ["cherry" , "lemon" , "7" , " bar"]
+print(random.choice(slotmachine))
+random.shuffle(slotmachine)
+print(random.choice(slotmachine))
+random.shuffle(slotmachine)
+print(random.choice(slotmachine))
+random.shuffle(slotmachine)
+
+
+
