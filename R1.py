6b869f601b6e94d99b3b81e3913e2c354543ce2c
Ashton Gracey <ag127064@student.wpsok.org>
2026-03-23 13:02:57 -0500

    R1

---
+++ R1.py
@@ -0,0 +1,36 @@
+#Name:Ashton Gracey
+#Class: 5th Hour
+#Assignment: HW-R1
+
+#1. Print "Hello World!"
+print("hello World!")
+#2. Create three variables with strings as values.
+me = "wall"
+desk = "long"
+cord = "computer"
+#3. Create three variables with booleans as values.
+wifi = True
+mouse = False
+light = True
+#4. Create three variables with integers as values.
+people = 11
+tv = 2
+chairs = 20
+#5. Create a variable that asks for the user's name and prints it.
+name = (input("What is your name?"))
+print(name)
+#6. Add together the three variables from #4 and print the result.
+add = (people + tv + chairs)
+print(add)
+#7. Subtract the second variable from the first from #4 and print the result.
+print(tv - people)
+#8. Multiply the third variable from #4 to the result of #6 and print the result.
+print(add * chairs)
+#9. Create a variable that asks the user for a number with a decimal (a float).
+dec = float(input("enter a number with a decimal"))
+print(dec)
+#10. Round the variable from #9 to the nearest whole number and print the result.
+print(round(dec))
+#11. Find the remainder (hint: modulo) of 355/113. Print the result.
+print(355%113)
+
